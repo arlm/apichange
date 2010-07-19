da1c76b30a2b60e29d658ae543bba0e4946ae2cf
Alois Kraus <akraus1@gmx.de>
2010-07-19 23:12:16 +0200

    Incremented version number

--- ApiChange.Api/src/Properties/AssemblyInfo.cs
+++ ApiChange.Api/src/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: InternalsVisibleTo("ApiChange_uTest")]
 [assembly: InternalsVisibleTo("ApiChange_iTest")]
